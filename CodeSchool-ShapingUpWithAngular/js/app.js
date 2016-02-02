9430ce4e4c4ca235d550bdef15cafc58c0a9e26b
Digital Revolution <stephen.vandelinder@gmail.com>
2016-02-02 08:26:48 -0700

    Refactored application to remove logic from the view

--- CodeSchool-ShapingUpWithAngular/js/app.js
+++ CodeSchool-ShapingUpWithAngular/js/app.js
@@ -22,4 +22,16 @@
 		this.product = gems; 
     }); 
 
+    app.controller('PanelController', function(){
+    	this.tab = 1; 
+    	this.selectTab = function(setTab){
+    		this.tab = setTab; 
+    	};
+    	this.isSelected = function(checkTab){
+    		return this.tab === checkTab
+    	};
+    });
+
+
+
 })();
